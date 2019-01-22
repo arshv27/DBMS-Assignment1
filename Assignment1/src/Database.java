39e41092e5090ab491151983f133f886fe4a1d27
Arsh Verma <arshverma@iiitd.ac.in>
2019-01-22 15:47:12 +0530

    Starting building database as ArrayList of arrays. Needs to be fixed further.

--- Assignment1/src/Database.java
+++ Assignment1/src/Database.java
@@ -2,23 +2,29 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Database
 {
+	private ArrayList<String[]> DBarray = new ArrayList<String[]>();
 	protected void ReadFile(String s1) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new FileReader(s1));
 		String line = null;
 		while ((line = br.readLine()) != null) {
 			String[] values = line.split(",");
-			for (String str : values) {
-				System.out.println(str);
+			String[] row = new String[3];
+			for (int i=0; i<values.length; i++) {
+				row[i%3]=values[i];
+				if(i%3==2) {
+					DBarray.add(row);
+				}
 			}
 		}
+
 		br.close();
 	}
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException{
 		Database D1= new Database();
 		D1.ReadFile("Metadata.txt");
 	}
