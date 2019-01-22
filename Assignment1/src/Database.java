1397a6a4d610328cc890def890a0e89057e1812e
Arsh Verma <arshverma@iiitd.ac.in>
2019-01-22 15:51:22 +0530

    Parsing metadata file correctly now. Try to run to see output.

--- Assignment1/src/Database.java
+++ Assignment1/src/Database.java
@@ -21,7 +21,12 @@ public class Database
 				}
 			}
 		}
-
+		for (int i=0; i<DBarray.size(); i++) {
+			for (int j=0; j<3; j++) {
+				System.out.print(DBarray.get(i)[j]+" ");
+			}
+			System.out.println();
+		}
 		br.close();
 	}
 	public static void main(String[] args) throws IOException{
